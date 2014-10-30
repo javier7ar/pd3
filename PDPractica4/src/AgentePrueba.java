70b4e04186f62b7861c78bf9acf02bc8ff7cb8aa
Xavier <Xavier@pcxavier>
2014-10-30 19:24:40 -0300

    agente de prueba

--- PDPractica4/src/AgentePrueba.java
+++ PDPractica4/src/AgentePrueba.java
@@ -1,6 +1,14 @@
 import jade.core.Agent;
+import jade.core.Location;
 
 
 public class AgentePrueba extends Agent {
-
+	// Ejecutado por única vez en la creación
+	 public void setup() 
+	 { 
+	 Location origen = here();
+	 System.out.println("\n\nHola, agente con nombre local " + getLocalName());
+	 System.out.println("Y nombre completo... " + getName());
+	 System.out.println("Y en location " + origen.getID() + "\n\n"); 
+	 }
 }
