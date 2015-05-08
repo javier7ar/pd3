ca1c9fcaa411df5e29243a1a1293a97c69eecd27
lilauth <lilauth@lilauth-Satellite-L305>
2015-05-08 18:44:52 -0300

    sólo cliente RMI

--- PDTrabajoFinal/src/ClienteRMI.java
+++ PDTrabajoFinal/src/ClienteRMI.java
@@ -5,6 +5,7 @@ import java.rmi.registry.Registry;
 public class ClienteRMI {
 	public static void main(String[] args)
 	{
+		String servidor;
 	/* Look for hostname and msg length in the command line */
 		if (args.length != 1){
 			System.out.println("1 argument needed: (remote) hostname");
@@ -12,10 +13,16 @@ public class ClienteRMI {
 		}
 		try {
 				String rname = "//" + args[0] + ":" + Registry.REGISTRY_PORT + "/remote";
-				IServidorRMI remote = (IServidorRMI) Naming.lookup(rname);
+				IServidorDirectorio remoto = (IServidorDirectorio) Naming.lookup(rname);
+				servidor = remoto.solicitarServidor();
+				
+				String sf= "//" + servidor + ":" + Registry.REGISTRY_PORT + "/remote";
+				IServidorRMI servidorFinal = (IServidorRMI) Naming.lookup(sf);
+				
 				int bufferlength = 100;
 				byte[] buffer = new byte[bufferlength];							
-				remote.sendThisBack(buffer);
+				servidorFinal.sendThisBack(buffer);
+				
 				System.out.println("Done");
 			} catch (Exception e) {
 				e.printStackTrace();
